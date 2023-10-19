6ea6019ee0f25fbc782b9e185bb36f2c32a84b5b
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-10-19 10:28:45 -0500

    Update

---
+++ SapoHp/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace SapoHp
+{
+    partial class Fondo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cisterna = new System.Windows.Forms.PictureBox();
+            this.carronegro = new System.Windows.Forms.PictureBox();
+            this.carrorojo = new System.Windows.Forms.PictureBox();
+            this.retroescavadora = new System.Windows.Forms.PictureBox();
+            this.moto = new System.Windows.Forms.PictureBox();
+            this.patrulla1 = new System.Windows.Forms.PictureBox();
+            this.patrulla2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.taxi = new System.Windows.Forms.PictureBox();
+            this.ranafrente = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cisterna)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carronegro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carrorojo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.retroescavadora)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patrulla1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patrulla2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taxi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranafrente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cisterna
+            // 
+            this.cisterna.BackColor = System.Drawing.Color.Transparent;
+            this.cisterna.Image = global::SapoHp.Properties.Resources.cisterna;
+            this.cisterna.Location = new System.Drawing.Point(1104, 339);
+            this.cisterna.Name = "cisterna";
+            this.cisterna.Size = new System.Drawing.Size(208, 68);
+            this.cisterna.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cisterna.TabIndex = 0;
+            this.cisterna.TabStop = false;
+            // 
+            // carronegro
+            // 
+            this.carronegro.BackColor = System.Drawing.Color.Transparent;
+            this.carronegro.Image = global::SapoHp.Properties.Resources.carD2;
+            this.carronegro.Location = new System.Drawing.Point(341, 433);
+            this.carronegro.Name = "carronegro";
+            this.carronegro.Size = new System.Drawing.Size(128, 61);
+            this.carronegro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carronegro.TabIndex = 1;
+            this.carronegro.TabStop = false;
+            // 
+            // carrorojo
+            // 
+            this.carrorojo.BackColor = System.Drawing.Color.Transparent;
+            this.carrorojo.Image = global::SapoHp.Properties.Resources.carro1;
+            this.carrorojo.Location = new System.Drawing.Point(830, 433);
+            this.carrorojo.Name = "carrorojo";
+            this.carrorojo.Size = new System.Drawing.Size(118, 61);
+            this.carrorojo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.carrorojo.TabIndex = 2;
+            this.carrorojo.TabStop = false;
+            // 
+            // retroescavadora
+            // 
+            this.retroescavadora.BackColor = System.Drawing.Color.Transparent;
+            this.retroescavadora.Image = global::SapoHp.Properties.Resources.retro;
+            this.retroescavadora.Location = new System.Drawing.Point(599, 338);
+            this.retroescavadora.Name = "retroescavadora";
+            this.retroescavadora.Size = new System.Drawing.Size(119, 69);
+            this.retroescavadora.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.retroescavadora.TabIndex = 3;
+            this.retroescavadora.TabStop = false;
+            // 
+            // moto
+            // 
+            this.moto.BackColor = System.Drawing.Color.Transparent;
+            this.moto.Image = global::SapoHp.Properties.Resources.moto;
+            this.moto.Location = new System.Drawing.Point(712, 699);
+            this.moto.Name = "moto";
+            this.moto.Size = new System.Drawing.Size(119, 61);
+            this.moto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.moto.TabIndex = 4;
+            this.moto.TabStop = false;
+            // 
+            // patrulla1
+            // 
+            this.patrulla1.BackColor = System.Drawing.Color.Transparent;
+            this.patrulla1.Image = global::SapoHp.Properties.Resources.carro3;
+            this.patrulla1.Location = new System.Drawing.Point(157, 590);
+            this.patrulla1.Name = "patrulla1";
+            this.patrulla1.Size = new System.Drawing.Size(162, 69);
+            this.patrulla1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.patrulla1.TabIndex = 6;
+            this.patrulla1.TabStop = false;
+            // 
+            // patrulla2
+            // 
+            this.patrulla2.BackColor = System.Drawing.Color.Transparent;
+            this.patrulla2.Image = global::SapoHp.Properties.Resources.carro311;
+            this.patrulla2.Location = new System.Drawing.Point(842, 599);
+            this.patrulla2.Name = "patrulla2";
+            this.patrulla2.Size = new System.Drawing.Size(140, 60);
+            this.patrulla2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.patrulla2.TabIndex = 7;
+            this.patrulla2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // taxi
+            // 
+            this.taxi.BackColor = System.Drawing.Color.Transparent;
+            this.taxi.Image = global::SapoHp.Properties.Resources.taxi;
+            this.taxi.Location = new System.Drawing.Point(175, 685);
+            this.taxi.Name = "taxi";
+            this.taxi.Size = new System.Drawing.Size(133, 78);
+            this.taxi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.taxi.TabIndex = 8;
+            this.taxi.TabStop = false;
+            // 
+            // ranafrente
+            // 
+            this.ranafrente.BackColor = System.Drawing.Color.Transparent;
+            this.ranafrente.Image = global::SapoHp.Properties.Resources.ranaFrente;
+            this.ranafrente.Location = new System.Drawing.Point(513, 769);
+            this.ranafrente.Name = "ranafrente";
+            this.ranafrente.Size = new System.Drawing.Size(47, 50);
+            this.ranafrente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ranafrente.TabIndex = 9;
+            this.ranafrente.TabStop = false;
+            // 
+            // Fondo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SapoHp.Properties.Resources.fondoVIDAS2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1324, 816);
+            this.Controls.Add(this.ranafrente);
+            this.Controls.Add(this.taxi);
+            this.Controls.Add(this.patrulla2);
+            this.Controls.Add(this.patrulla1);
+            this.Controls.Add(this.moto);
+            this.Controls.Add(this.retroescavadora);
+            this.Controls.Add(this.carrorojo);
+            this.Controls.Add(this.carronegro);
+            this.Controls.Add(this.cisterna);
+            this.DoubleBuffered = true;
+            this.Name = "Fondo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Fondo_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.cisterna)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carronegro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carrorojo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.retroescavadora)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patrulla1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patrulla2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taxi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ranafrente)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cisterna;
+        private System.Windows.Forms.PictureBox carronegro;
+        private System.Windows.Forms.PictureBox carrorojo;
+        private System.Windows.Forms.PictureBox retroescavadora;
+        private System.Windows.Forms.PictureBox moto;
+        private System.Windows.Forms.PictureBox patrulla1;
+        private System.Windows.Forms.PictureBox patrulla2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox taxi;
+        private System.Windows.Forms.PictureBox ranafrente;
+    }
+}
+
---
+++ SapoHp/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SapoHp
+{
+    public partial class Fondo : Form
+    {
+        public Fondo()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            //Funcion de Desplazamiento
+            moverCarros();
+
+            //Reaparicon Objetos
+            // Der - Izq
+            retorno1(patrulla1);
+            retorno1(patrulla2);
+            retorno1(cisterna);
+            retorno1(retroescavadora);
+
+            // Izq - Der
+            retornocarrito(taxi);
+            retornocarrito(carronegro);
+            retornocarrito(carrorojo);
+            retornocarrito(moto);
+
+            if (ranafrente.Bounds.IntersectsWith(cisterna.Bounds))
+            {
+                ranafrente.Image = SapoHp.Properties.Resources.ranaAplastada;
+            }
+        }
+
+        // Funcion Reaparicion  Izq - Der
+        public void retornocarrito(PictureBox carrito)
+        {
+            if (carrito.Location.X > 1300)
+            {
+                carrito.Location = new Point(-300, carrito.Location.Y);
+            }
+        }
+        // Funcion Reaparicion  Der - Izq
+        public void retorno1(PictureBox carrito)
+        {
+            if (carrito.Location.X < -210)
+            {
+                carrito.Location = new Point(1400, carrito.Location.Y);
+            }
+        }
+
+        //Funcion Movimiento Objetos
+        public void moverCarros()
+        {
+            carronegro.Left = carronegro.Left + 10;
+            carrorojo.Left = carrorojo.Left + 10;
+            retroescavadora.Left = retroescavadora.Left - 10;
+            cisterna.Left = cisterna.Left - 10;
+            taxi.Left = taxi.Left + 10;
+            moto.Left = moto.Left + 10;
+            patrulla1.Left = patrulla1.Left - 10;
+            patrulla2.Left = patrulla2.Left - 10;
+        }
+
+        //Funcion de Movimiento de la Rana
+        private void Fondo_KeyDown(object sender, KeyEventArgs evento)
+        {
+            //Hacia la Derecha
+            if(evento.KeyCode == Keys.Right)
+            {
+                ranafrente.Left += 10;
+                // Orientazcion de Imagen respecto a la orientacion del movimiento
+                ranafrente.Image = SapoHp.Properties.Resources.ranaDere;
+            }
+
+            // Hacia la Izquierda
+            if (evento.KeyCode == Keys.Left)
+            {
+                ranafrente.Left -= 10;
+                // Orientazcion de Imagen respecto a la orientacion del movimiento
+                ranafrente.Image = SapoHp.Properties.Resources.ranaIzq;
+            }
+
+            // Hacia Arriba
+            if (evento.KeyCode == Keys.Up)
+            {
+                ranafrente.Top -= 10;
+                // Orientazcion de Imagen respecto a la orientacion del movimiento
+                ranafrente.Image = SapoHp.Properties.Resources.ranaFrente;
+            }
+
+            //Hacia Abajo
+            if (evento.KeyCode == Keys.Down)
+            {
+                ranafrente.Top += 10;
+                // Orientazcion de Imagen respecto a la orientacion del movimiento
+                ranafrente.Image = SapoHp.Properties.Resources.ranaContraria;
+            }
+        }
+    }
+}
