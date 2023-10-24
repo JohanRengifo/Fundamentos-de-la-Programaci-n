6bd99128288654cbb5a400814e0c7b33229432a1
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-10-24 08:19:05 -0500

    Update

--- Java/Ciclos/Ciclo While/NmPar.java
+++ Java/Ciclos/Ciclo While/NmPar.java
@@ -1,81 +1,76 @@
-
 import java.util.Scanner;
 
-class NumerosPrimos
-{
+class NumerosPrimos {
     static Scanner digite = new Scanner(System.in);
-    public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         int opcion = 0;
-        do{
+        do {
             System.out.println("\n 1...PRIMOS \n 2...Tabla \n 3...Fibonacci \n 4...Salir \n");
             opcion = digite.nextInt();
-            switch(opcion)
-            {
-                case 1: 
+            switch (opcion) {
+                case 1:
                     numeroPrimo();
-                break;
+                    break;
 
                 case 2:
                     tablas();
-                break;
+                    break;
 
                 case 3:
-                        int n = 50;
-                        int resultado=calcularFibonacci(n);
-                        System.out.println("El Numero Fibonacci esta en la Posicion " + n + " y su resultado es: " + resultado);
-                break;
+                    int n = 50;
+                    int resultado = calcularFibonacci(n);
+                    System.out.println("El Numero Fibonacci en la Posición " + n + " es: " + resultado);
+                    break;
 
                 case 4:
                     System.out.println("Cierre de Programa");
-                break;
+                    break;
 
                 default:
-                    System.out.println("Ingreso mal la Opcion.");
+                    System.out.println("Ingresó una opción incorrecta.");
             }
-        }while(opcion!=4);
+        } while (opcion != 4);
     }
-    public static void numeroPrimo()
-    {
-        int num, k_primates=0;
+
+    public static void numeroPrimo() {
+        int num, k_primos = 0;
         // Solicita el  Numero
         System.out.println("Por Favor Ingrese un Numero:");
         num = digite.nextInt();
 
         // Encuentra los divisores del número
         for (int k = 1; k <= num; k++) {
-            if((num % k)== 0)
-            {
-                k_primates++;
+            if (num % k == 0) {
+                k_primos++;
             }
         }
-        if(k_primates == 2){
-            System.out.println("Es Prim, Sus divisores son: " + k_primates);
-        }else{
-            System.out.println("No es Primo, Sus divisores son: " + k_primates);
+        if (k_primos == 2) {
+            System.out.println("Es Primo, sus divisores son: " + k_primos);
+        } else {
+            System.out.println("No es Primo, sus divisores son: " + k_primos);
         }
     }
 
-    public static void tablas(){
-        int num, rest = 0;
+    public static void tablas() {
+        int num, result;
         // Solicita el  Numero
         System.out.println("Por Favor Ingrese un Numero:");
         num = digite.nextInt();
 
         System.out.println("La Tabla de Multiplicar del " + num + " es: ");
 
-        for(int i = 1; i <= 10; i++) {
-            rest = num * i;
-            System.out.println(num + " X " + i + ": " + rest);
+        for (int i = 1; i <= 10; i++) {
+            result = num * i;
+            System.out.println(num + " X " + i + ": " + result);
         }
     }
 
-    public static void calcularFibonacci(int n){
-        
-        if(n<=1){
+    public static int calcularFibonacci(int n) {
+        if (n <= 1) {
             return n;
-        }else{
-            return calcularFibonacci(n- 1) + calcularFibonacci(n - 2);
+        } else {
+            return calcularFibonacci(n - 1) + calcularFibonacci(n - 2);
         }
     }
 }
