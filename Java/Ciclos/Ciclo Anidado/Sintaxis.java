66ffbc68e8a2d1d10011c1f70300cd7115a4911a
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-10-27 11:21:28 -0500

    Update

---
+++ Java/Ciclos/Ciclo Anidado/Sintaxis.java
@@ -0,0 +1,9 @@
+class CicloTablaMult{
+    public static void main(String Args[]){
+        int tabla;
+        //Ciclo
+        for(tabla = 1; tabla <= 10; tabla++){
+            System.out.println("Tabla: " + tabla)
+        }
+    }
+}
