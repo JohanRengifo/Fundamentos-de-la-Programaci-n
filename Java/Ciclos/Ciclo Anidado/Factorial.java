78eb3ac03679b3277d3be612ab025f5a23b38910
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-10-27 17:41:02 +0000

    Update

--- Java/Ciclos/Ciclo Anidado/Factorial.java
+++ Java/Ciclos/Ciclo Anidado/Factorial.java
@@ -1,5 +1,12 @@
 class Factorial {
     public static void main(String args[]){
-        
+        int factorial = 1;
+        for(int numero = 1; numero <= 15; numero++){
+            factorial = 1;
+            for(int n = 1; n <= numero; n++){
+                factorial = factorial * n;
+            }
+            System.out.println("El Factorial de " + numero + "! es " + factorial);
+        }
     }
 }
