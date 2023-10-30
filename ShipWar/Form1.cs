151fb9a5a4b54ff6d765d00da5c0cc4c0428ba20
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-10-30 17:59:07 -0500

    Beta

--- ShipWar/Form1.cs
+++ ShipWar/Form1.cs
@@ -1,136 +1,160 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace SapoHp
 {
     public partial class Fondo : Form
     {
-        // Vidas
         int vidas = 3;
-
-        // Musica
-        SoundPlayer musica_fondo = new SoundPlayer(@"C:\");
-        SoundPlayer colision = new SoundPlayer(@"C:\");
+        int puntos = 0; // Puntos iniciales
+        int tiempoTranscurrido = 0; // Segundos transcurridos
+        int intentosRestantes = 3; // Número de intentos restantes
+        SoundPlayer musica_fondo = new SoundPlayer(@".wav");
+        SoundPlayer colision = new SoundPlayer(@".wav"); 
+        Timer puntosTimer = new Timer();
 
         public Fondo()
         {
             InitializeComponent();
-            musica_fondo.Play(); // Reproduce la Musica de Fondo
+            musica_fondo.Play(); // Reproduce la música de fondo
+            puntosTimer.Interval = 1000; // 1 segundo
+            puntosTimer.Tick += PuntosTimer_Tick;
+            puntosTimer.Start();
         }
 
-        private void timer1_Tick_1(object sender, EventArgs e)
+        private void PuntosTimer_Tick(object sender, EventArgs e)
         {
-            //Funcion de Desplazamiento
-            moverCarros();
+            tiempoTranscurrido++;
 
-            //Reaparicon Objetos
-            // Der - Izq
-            retorno1(patrulla1);
-
-            // Izq - Der
-            retornocarrito(taxi);
-
-            // Choque de la Rana
-            choqueRana(cisterna);
-            choqueRana(taxi);
-        }
-
-        // Funcion Cambio Imagen | Choque
-        public void choqueRana(PictureBox choque)
-        {
-            if (sapa.Bounds.IntersectsWith(choque.Bounds))
+            if (tiempoTranscurrido % 60 == 0)
             {
-                barco.Image = ShipWar.Properties.Resources.barcodestruido;
+                puntos += 45; // Cada 60 segundos, se suman 45 puntos
+            }
+            else if (tiempoTranscurrido % 24 == 0)
+            {
+                puntos =+20; //Cada 24 segundos, se suman 20 puntos
+            }
+            else if (tiempoTranscurrido % 16 == 0)
+            {
+                puntos += 14; // Cada 16 segundos, se suman 14 puntos
+            }
+            else if (tiempoTranscurrido % 8 == 0)
+            {
+                puntos += 10; // Cada 8 segundos, se suman 10 puntos
+            }
+
+            // Actualiza la etiqueta de puntos en la interfaz de usuario
+            puntosLabel.Text = "Puntos: " + puntos;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            desplazamiento();
+            obstaculosEO(obstaculos);
+            obstaculosOE(obstaculos);
+            colision(obstaculos); // Colisión con Obstaculos
+        }
+
+        public void colision(PictureBox colision)
+        {
+            if (barco.Bounds.IntersectsWith(colision.Bounds))
+            {
+                barco.Image = ShipWar.Properties.Resources.barcodestroy;
                 timer1.Stop();
                 colision.Play();
                 quitarVidas();
+                if (vidas > 0)
+                {
+                    if (intentosRestantes > 0)
+                    {
+                        intentosRestantes--;
+                        DialogResult result = MessageBox.Show("¿Deseas reiniciar el juego?", "Game Over",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.Yes)
+                        {
+                            ReiniciarJuego();
+                        }
+                        else
+                        {
+                            timer1.Stop();
+                        }
+                    }
+                }
             }
-            
         }
 
-        // Funcion Quita Vidas
         public void quitarVidas()
         {
             switch (vidas)
             {
                 case 1:
-                    corazon1.Image = ShipWar.Properties.Resources.calavera; // Quita el Corazon por una Calavera
-                break;
+                    corazon1.Image = ShipWar.Properties.Resources.calavera;
+                    break;
                 case 2:
                     corazon2.Image = ShipWar.Properties.Resources.calavera;
-                break;
+                    break;
                 case 3:
                     corazon3.Image = ShipWar.Properties.Resources.calavera;
-                break;
+                    break;
             }
             vidas--;
         }
 
-        // Funcion Reaparicion  Izq - Der
-        public void retornocarrito(PictureBox carrito)
+        public void obstaculosOE(PictureBox obstaculos)
         {
-            if (carrito.Location.X > 1300)
+            if (obstaculos.Location.X > 1300)
             {
-                carrito.Location = new Point(-300, carrito.Location.Y);
-            }
-        }
-        // Funcion Reaparicion  Der - Izq
-        public void retorno1(PictureBox carrito)
-        {
-            if (carrito.Location.X < -210)
-            {
-                carrito.Location = new Point(1400, carrito.Location.Y);
+                obstaculos.Location = new Point(-300, obstaculos.Location.Y);
             }
         }
 
-        //Funcion Movimiento Objetos
-        public void moverCarros()
+        public void obstaculosEO(PictureBox obstaculos)
         {
-            carronegro.Left = carronegro.Left + 10;
-            
+            if (obstaculos.Location.X < -210)
+            {
+                obstaculos.Location = new Point(1400, obstaculos.Location.Y);
+            }
         }
 
-        //Funcion de Movimiento de la Rana
+        public void desplazamiento()
+        {
+            barco.Left = barco.Left + 12;
+        }
+
         private void Fondo_KeyDown(object sender, KeyEventArgs evento)
         {
-            //Hacia la Derecha
-            if(evento.KeyCode == Keys.Right)
-            {
-                barco.Left += 10;
-                // Orientazcion de Imagen respecto a la orientacion del movimiento
-                ranafrente.Image = ShipWar.Properties.Resources.ranaDere;
-            }
-
-            // Hacia la Izquierda
-            if (evento.KeyCode == Keys.Left)
-            {
-                barco.Left -= 10;
-                // Orientazcion de Imagen respecto a la orientacion del movimiento
-                barco.Image = ShipWar.Properties.Resources.ranaIzq;
-            }
-
             // Hacia Arriba
             if (evento.KeyCode == Keys.Up)
             {
-                ranafrente.Top -= 10;
-                // Orientazcion de Imagen respecto a la orientacion del movimiento
-                barco.Image = ShipWar.Properties.Resources.ranaFrente;
+                barco.Top -= 10;
+                barco.Image = ShipWar.Properties.Resources.barco;
             }
 
-            //Hacia Abajo
+            // Hacia Abajo
             if (evento.KeyCode == Keys.Down)
             {
-                ranafrente.Top += 10;
-                // Orientazcion de Imagen respecto a la orientacion del movimiento
-                barco.Image = ShipWar.Properties.Resources.ranaContraria;
+                barco.Top += 10;
+                barco.Image = ShipWar.Properties.Resources.barco;
             }
+        }
+
+        // Función para reiniciar el juego
+        private void ReiniciarJuego()
+        {
+            // Restablece el estado del juego
+            vidas = 3;
+            puntos = 0;
+            intentosRestantes = 3;
+            tiempoTranscurrido = 0;
+            puntosLabel.Text = "Puntos: 0";
+            corazon1.Image = ShipWar.Properties.Resources.corazon; // Restablece las vidas
+            corazon2.Image = ShipWar.Properties.Resources.corazon;
+            corazon3.Image = ShipWar.Properties.Resources.corazon;
+            barco.Location = new Point(inicioX, inicioY); // Restablece la posición del barco
+            barco.Image = ShipWar.Properties.Resources.barco; // Restablece la imagen del barco
+            timer1.Start(); // Reinicia el temporizador
         }
     }
 }
