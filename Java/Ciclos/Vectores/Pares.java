243da859c72edbf6c106e234259a749b0dbc2946
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-11-10 18:03:06 +0000

    Beta

--- Java/Ciclos/Vectores/Pares.java
+++ Java/Ciclos/Vectores/Pares.java
@@ -1,29 +1,31 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
-class Taller3
-{
-    public static void main(String args[]){
+public class Pares {
+    public static int countEvenNumbers() {
         Scanner digite = new Scanner(System.in);
         Random aleatorio  = new Random();
-        
+
         int numero[] = new int[100];
         int k_repite = 0;
-        
-        for(int pos= 0; pos<100; pos++){
 
+        for(int pos= 0; pos<100; pos++){
             numero[pos] = aleatorio.nextInt(11);
             System.out.println("Posicion = [" + pos + "] Numero: " + numero[pos]);
-            
         }
 
         for(int pos = 0; pos < 100; pos++){
             if ((numero[pos] % 2)==0){
                 k_repite++;
-                System.out.println("Numero Par " + numero);
+                System.out.println("Numero Par " + numero[pos]);
             }
         }
 
         System.out.println("Cuantos Son: " + k_repite);
+        return k_repite;
+    }
+
+    public static void main(String args[]){
+        countEvenNumbers();
     }
 }
