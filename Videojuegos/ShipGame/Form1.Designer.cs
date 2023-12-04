7515ae9d7e035599f328b20d192817c034ffe2e8
Johan Rengifo <89481766+JohanRengifo@users.noreply.github.com>
2023-12-04 09:08:23 -0500

    Update

---
+++ Videojuegos/ShipGame/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿
+namespace ShipGame
+{
+    partial class Fondo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.puntosLabel = new System.Windows.Forms.Label();
+            this.moneda2 = new System.Windows.Forms.PictureBox();
+            this.moneda1 = new System.Windows.Forms.PictureBox();
+            this.moneda = new System.Windows.Forms.PictureBox();
+            this.tiburon2 = new System.Windows.Forms.PictureBox();
+            this.tiburon1 = new System.Windows.Forms.PictureBox();
+            this.islasola2 = new System.Windows.Forms.PictureBox();
+            this.islasola3 = new System.Windows.Forms.PictureBox();
+            this.islasola4 = new System.Windows.Forms.PictureBox();
+            this.islasola1 = new System.Windows.Forms.PictureBox();
+            this.islarocosa3 = new System.Windows.Forms.PictureBox();
+            this.islarocosa2 = new System.Windows.Forms.PictureBox();
+            this.islarocosa4 = new System.Windows.Forms.PictureBox();
+            this.islarocosa1 = new System.Windows.Forms.PictureBox();
+            this.roca2 = new System.Windows.Forms.PictureBox();
+            this.roca3 = new System.Windows.Forms.PictureBox();
+            this.roca4 = new System.Windows.Forms.PictureBox();
+            this.roca1 = new System.Windows.Forms.PictureBox();
+            this.corazon2 = new System.Windows.Forms.PictureBox();
+            this.corazon1 = new System.Windows.Forms.PictureBox();
+            this.lancha = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.moneda2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moneda1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moneda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiburon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiburon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lancha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // puntosLabel
+            // 
+            this.puntosLabel.AutoSize = true;
+            this.puntosLabel.BackColor = System.Drawing.SystemColors.Info;
+            this.puntosLabel.Location = new System.Drawing.Point(93, 9);
+            this.puntosLabel.Name = "puntosLabel";
+            this.puntosLabel.Size = new System.Drawing.Size(46, 13);
+            this.puntosLabel.TabIndex = 0;
+            this.puntosLabel.Text = "Puntos: ";
+            // 
+            // moneda2
+            // 
+            this.moneda2.Image = global::ShipGame.Properties.Resources._7;
+            this.moneda2.Location = new System.Drawing.Point(986, 671);
+            this.moneda2.Name = "moneda2";
+            this.moneda2.Size = new System.Drawing.Size(40, 34);
+            this.moneda2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.moneda2.TabIndex = 20;
+            this.moneda2.TabStop = false;
+            // 
+            // moneda1
+            // 
+            this.moneda1.Image = global::ShipGame.Properties.Resources._4;
+            this.moneda1.Location = new System.Drawing.Point(1054, 101);
+            this.moneda1.Name = "moneda1";
+            this.moneda1.Size = new System.Drawing.Size(40, 34);
+            this.moneda1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.moneda1.TabIndex = 19;
+            this.moneda1.TabStop = false;
+            // 
+            // moneda
+            // 
+            this.moneda.Image = global::ShipGame.Properties.Resources._10;
+            this.moneda.Location = new System.Drawing.Point(248, 407);
+            this.moneda.Name = "moneda";
+            this.moneda.Size = new System.Drawing.Size(40, 34);
+            this.moneda.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.moneda.TabIndex = 18;
+            this.moneda.TabStop = false;
+            // 
+            // tiburon2
+            // 
+            this.tiburon2.Image = global::ShipGame.Properties.Resources.tiburon;
+            this.tiburon2.Location = new System.Drawing.Point(300, 846);
+            this.tiburon2.Name = "tiburon2";
+            this.tiburon2.Size = new System.Drawing.Size(83, 65);
+            this.tiburon2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tiburon2.TabIndex = 17;
+            this.tiburon2.TabStop = false;
+            // 
+            // tiburon1
+            // 
+            this.tiburon1.Image = global::ShipGame.Properties.Resources.tiburon;
+            this.tiburon1.Location = new System.Drawing.Point(193, 588);
+            this.tiburon1.Name = "tiburon1";
+            this.tiburon1.Size = new System.Drawing.Size(74, 61);
+            this.tiburon1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.tiburon1.TabIndex = 16;
+            this.tiburon1.TabStop = false;
+            // 
+            // islasola2
+            // 
+            this.islasola2.Image = global::ShipGame.Properties.Resources.islasola;
+            this.islasola2.Location = new System.Drawing.Point(128, 25);
+            this.islasola2.Name = "islasola2";
+            this.islasola2.Size = new System.Drawing.Size(216, 203);
+            this.islasola2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.islasola2.TabIndex = 15;
+            this.islasola2.TabStop = false;
+            // 
+            // islasola3
+            // 
+            this.islasola3.Image = global::ShipGame.Properties.Resources.islasola;
+            this.islasola3.Location = new System.Drawing.Point(465, 325);
+            this.islasola3.Name = "islasola3";
+            this.islasola3.Size = new System.Drawing.Size(206, 193);
+            this.islasola3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.islasola3.TabIndex = 14;
+            this.islasola3.TabStop = false;
+            // 
+            // islasola4
+            // 
+            this.islasola4.Image = global::ShipGame.Properties.Resources.islasola;
+            this.islasola4.Location = new System.Drawing.Point(667, 588);
+            this.islasola4.Name = "islasola4";
+            this.islasola4.Size = new System.Drawing.Size(195, 202);
+            this.islasola4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.islasola4.TabIndex = 13;
+            this.islasola4.TabStop = false;
+            // 
+            // islasola1
+            // 
+            this.islasola1.Image = global::ShipGame.Properties.Resources.islasola;
+            this.islasola1.Location = new System.Drawing.Point(831, 822);
+            this.islasola1.Name = "islasola1";
+            this.islasola1.Size = new System.Drawing.Size(209, 177);
+            this.islasola1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.islasola1.TabIndex = 12;
+            this.islasola1.TabStop = false;
+            // 
+            // islarocosa3
+            // 
+            this.islarocosa3.Image = global::ShipGame.Properties.Resources.islarocosa;
+            this.islarocosa3.Location = new System.Drawing.Point(918, 247);
+            this.islarocosa3.Name = "islarocosa3";
+            this.islarocosa3.Size = new System.Drawing.Size(214, 215);
+            this.islarocosa3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.islarocosa3.TabIndex = 11;
+            this.islarocosa3.TabStop = false;
+            // 
+            // islarocosa2
+            // 
+            this.islarocosa2.Image = global::ShipGame.Properties.Resources.islarocosa;
+            this.islarocosa2.Location = new System.Drawing.Point(1164, 526);
+            this.islarocosa2.Name = "islarocosa2";
+            this.islarocosa2.Size = new System.Drawing.Size(223, 208);
+            this.islarocosa2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.islarocosa2.TabIndex = 10;
+            this.islarocosa2.TabStop = false;
+            // 
+            // islarocosa4
+            // 
+            this.islarocosa4.Image = global::ShipGame.Properties.Resources.islarocosa;
+            this.islarocosa4.Location = new System.Drawing.Point(1221, 822);
+            this.islarocosa4.Name = "islarocosa4";
+            this.islarocosa4.Size = new System.Drawing.Size(246, 207);
+            this.islarocosa4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.islarocosa4.TabIndex = 9;
+            this.islarocosa4.TabStop = false;
+            // 
+            // islarocosa1
+            // 
+            this.islarocosa1.Image = global::ShipGame.Properties.Resources.islarocosa;
+            this.islarocosa1.Location = new System.Drawing.Point(629, 0);
+            this.islarocosa1.Name = "islarocosa1";
+            this.islarocosa1.Size = new System.Drawing.Size(233, 200);
+            this.islarocosa1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.islarocosa1.TabIndex = 8;
+            this.islarocosa1.TabStop = false;
+            // 
+            // roca2
+            // 
+            this.roca2.Image = global::ShipGame.Properties.Resources.roca;
+            this.roca2.Location = new System.Drawing.Point(918, 468);
+            this.roca2.Name = "roca2";
+            this.roca2.Size = new System.Drawing.Size(90, 50);
+            this.roca2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roca2.TabIndex = 7;
+            this.roca2.TabStop = false;
+            // 
+            // roca3
+            // 
+            this.roca3.Image = global::ShipGame.Properties.Resources.roca;
+            this.roca3.Location = new System.Drawing.Point(629, 206);
+            this.roca3.Name = "roca3";
+            this.roca3.Size = new System.Drawing.Size(90, 50);
+            this.roca3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roca3.TabIndex = 6;
+            this.roca3.TabStop = false;
+            // 
+            // roca4
+            // 
+            this.roca4.Image = global::ShipGame.Properties.Resources.roca;
+            this.roca4.Location = new System.Drawing.Point(416, 46);
+            this.roca4.Name = "roca4";
+            this.roca4.Size = new System.Drawing.Size(90, 50);
+            this.roca4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roca4.TabIndex = 5;
+            this.roca4.TabStop = false;
+            // 
+            // roca1
+            // 
+            this.roca1.Image = global::ShipGame.Properties.Resources.roca;
+            this.roca1.Location = new System.Drawing.Point(1164, 740);
+            this.roca1.Name = "roca1";
+            this.roca1.Size = new System.Drawing.Size(90, 50);
+            this.roca1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roca1.TabIndex = 4;
+            this.roca1.TabStop = false;
+            // 
+            // corazon2
+            // 
+            this.corazon2.Image = global::ShipGame.Properties.Resources.corazon;
+            this.corazon2.Location = new System.Drawing.Point(49, 9);
+            this.corazon2.Name = "corazon2";
+            this.corazon2.Size = new System.Drawing.Size(38, 31);
+            this.corazon2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.corazon2.TabIndex = 3;
+            this.corazon2.TabStop = false;
+            // 
+            // corazon1
+            // 
+            this.corazon1.Image = global::ShipGame.Properties.Resources.corazon;
+            this.corazon1.Location = new System.Drawing.Point(12, 9);
+            this.corazon1.Name = "corazon1";
+            this.corazon1.Size = new System.Drawing.Size(38, 31);
+            this.corazon1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.corazon1.TabIndex = 2;
+            this.corazon1.TabStop = false;
+            // 
+            // lancha
+            // 
+            this.lancha.BackColor = System.Drawing.Color.Transparent;
+            this.lancha.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lancha.Image = global::ShipGame.Properties.Resources.ship;
+            this.lancha.Location = new System.Drawing.Point(10, 373);
+            this.lancha.Margin = new System.Windows.Forms.Padding(1);
+            this.lancha.Name = "lancha";
+            this.lancha.Size = new System.Drawing.Size(75, 41);
+            this.lancha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.lancha.TabIndex = 1;
+            this.lancha.TabStop = false;
+            // 
+            // Fondo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1479, 1041);
+            this.Controls.Add(this.moneda2);
+            this.Controls.Add(this.moneda1);
+            this.Controls.Add(this.moneda);
+            this.Controls.Add(this.tiburon2);
+            this.Controls.Add(this.tiburon1);
+            this.Controls.Add(this.islasola2);
+            this.Controls.Add(this.islasola3);
+            this.Controls.Add(this.islasola4);
+            this.Controls.Add(this.islasola1);
+            this.Controls.Add(this.islarocosa3);
+            this.Controls.Add(this.islarocosa2);
+            this.Controls.Add(this.islarocosa4);
+            this.Controls.Add(this.islarocosa1);
+            this.Controls.Add(this.roca2);
+            this.Controls.Add(this.roca3);
+            this.Controls.Add(this.roca4);
+            this.Controls.Add(this.roca1);
+            this.Controls.Add(this.corazon2);
+            this.Controls.Add(this.corazon1);
+            this.Controls.Add(this.lancha);
+            this.Controls.Add(this.puntosLabel);
+            this.Name = "Fondo";
+            this.Text = "Fondo";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Fondo_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.moneda2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moneda1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moneda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiburon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tiburon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islasola1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.islarocosa1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roca1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.corazon1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lancha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label puntosLabel;
+        private System.Windows.Forms.PictureBox lancha;
+        private System.Windows.Forms.PictureBox corazon1;
+        private System.Windows.Forms.PictureBox corazon2;
+        private System.Windows.Forms.PictureBox roca1;
+        private System.Windows.Forms.PictureBox roca4;
+        private System.Windows.Forms.PictureBox roca3;
+        private System.Windows.Forms.PictureBox roca2;
+        private System.Windows.Forms.PictureBox islarocosa1;
+        private System.Windows.Forms.PictureBox islarocosa4;
+        private System.Windows.Forms.PictureBox islarocosa2;
+        private System.Windows.Forms.PictureBox islarocosa3;
+        private System.Windows.Forms.PictureBox islasola1;
+        private System.Windows.Forms.PictureBox islasola4;
+        private System.Windows.Forms.PictureBox islasola3;
+        private System.Windows.Forms.PictureBox islasola2;
+        private System.Windows.Forms.PictureBox tiburon1;
+        private System.Windows.Forms.PictureBox tiburon2;
+        private System.Windows.Forms.PictureBox moneda;
+        private System.Windows.Forms.PictureBox moneda1;
+        private System.Windows.Forms.PictureBox moneda2;
+    }
+}
+
